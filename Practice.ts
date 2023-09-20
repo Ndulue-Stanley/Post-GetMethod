5091e2bde07389ec03ede6de7f39d76510777963
Ndulue-Stanley <stanleycsebastine@gmail.com>
2023-09-20 02:42:13 +0100

    Practice3

--- Practice.ts
+++ Practice.ts
@@ -1,79 +1,95 @@
+import http, {ServerResponse, IncomingMessage} from 'http'
 
-import http, { IncomingMessage, ServerResponse } from "http";
-import event from "events";
-const port: number = 7587;
+
+const port: number = 5294;
 
 interface iData {
-  id: number;
-  name: string;
-  phone: number;
-  stack: string;
-}
-
-interface iMessage {
-  message: string;
-  success: boolean;
-  data: null | {} | {}[];
-}
-
-const set08: iData[] = [
-  {
-    id: 1,
-    name: "joan",
-    phone: 8023474637,
-    stack: "Full-Stack",
-  },
-  {
-    id: 2,
-    name: "Habib",
-    phone: 8023474637,
-    stack: "Full-Stack",
-  },
+    id: number;
+    name: string;
+    phone: number;
+    stack: string;
+  }
   
-];
+  interface iMessage {
+    message: string;
+    success: boolean;
+  data: null | {} | {}[];
+  }  
 
-const server = http.createServer(
-  (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
-    res.setHeader("Content-Type", "Application/Json");
-    const { method, url } = req;
+  const set08: iData[] = [
+    {
+      id: 1,
+      name: "joan",
+      phone: 8023474637,
+      stack: "Full-Stack",
+    },
+    {
+      id: 2,
+      name: "Habib",
+      phone: 8023474637,
+      stack: "Full-Stack",
+    },
+    {
+      id: 3,
+      name: "Daniel",
+      phone: 8023474637,
+      stack: "Full-Stack",
+    },
+    {
+      id: 4,
+      name: "SEAN",
+      phone: 8023474637,
+      stack: "Full-Stack",
+    },
+    {
+      id: 5,
+      name: "joan",
+      phone: 8023474637,
+      stack: "Full-Stack",
+    },
+  ];
+
+const Server = http.createServer((req:IncomingMessage, res: ServerResponse<IncomingMessage>)=>{
+    res.setHeader('Content-type', 'Application/Json');
+    const {method, url} = req
     let status: number = 404;
 
     let response: iMessage = {
-      message: "failed",
-      success: false,
-      data: null,
-    };
-    const container: any = [];
-    req
-      .on("data", (chunk: any) => {
-        container.push(chunk);
-      })
-      .on("end", () => {
-        if (url === "/" && method === "GET") {
-          status = 200;
-          response.message = "Success";
-          response.success = true;
-          response.data = set08;
-          res.write(JSON.stringify({ response, status }));
-          res.end();
-        }
+        message: 'Failure',
+        success: false,
+        data: null
+    }
 
+    const container: any = []
+
+    req.on('data', (chunk: any)=>{
+        container.push(chunk)
+    });
+    req.on('end', ()=>{
+        if(url === '/' && method === 'GET'){
+            status = 200
+            response.message = 'Done';
+            response.success = true;
+            response.data = set08;
+            res.write(JSON.stringify({response, status}))
+            res.end()
+
+            if( url === '/' && method === "POST"){
+                status = 201
+                const body = JSON.parse(container);
+                set08.push(body);
+                response.message = "SUCCESSFULLY added";
+                response.success = true;
+                response.data = set08;
+                res.write(JSON.stringify({ response, status }));
       
-        if (url === "/" && method === "POST") {
-          status = 201;
-          const body = JSON.parse(container);
-          set08.push(body);
-          response.message = "SUCCESSFULLY appended";
-          response.success = true;
-          response.data = set08;
-          res.write(JSON.stringify({ response, status }));
+                res.end();
 
-          res.end();
+            }
+            
+            Server.listen(port, () => {
+    console.log("Server is up and running");
+  });
         }
-      });
-  }
-);
-
-server.listen(port, () => {
-  console.log("Server is up and running");
-});
+    })
+})
