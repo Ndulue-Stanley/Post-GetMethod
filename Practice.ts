e436084e035a848b0aca33ee5077e90214f715ae
Ndulue-Stanley <stanleycsebastine@gmail.com>
2023-09-20 02:11:44 +0100

    Practice2

--- Practice.ts
+++ Practice.ts
@@ -1,75 +1,83 @@
-import http, {ServerResponse, IncomingMessage} from 'http'
-import fs from 'fs'
-import event from 'events'
 
-const port: number = 7464
+import http, { IncomingMessage, ServerResponse } from "http";
+const port: number = 9999;
 
 interface iData {
-    id: number,
-    name: string,
-    phone: number,
-    stack: string
+  id: number;
+  name: string;
+  phone: number;
+  stack: string;
 }
 
 interface iMessage {
-    mesage: string;
-    success: boolean
-    data: null | {}  | {}[]
-
+  message: string;
+  success: boolean;
+  data: null | {} | {}[];
 }
 
-const set07 : iData[] = [
-    {
-        id : 7,
-        name: 'Greater',
-        phone: 84498886373,
-        stack: 'Full-Stack'
-    }
-] 
+const set08: iData[] = [
+  {
+    id: 1,
+    name: "joan",
+    phone: 8023474637,
+    stack: "Full-Stack",
+  },
+  {
+    id: 2,
+    name: "Habib",
+    phone: 8023474637,
+    stack: "Full-Stack",
+  },
+  {
+    id: 3,
+    name: "Daniel",
+    phone: 8023474637,
+    stack: "Full-Stack",
+  }
+];
 
-const Server = http.createServer((req: IncomingMessage, res:ServerResponse<IncomingMessage> )=>{
-    res.setHeader("Content-Type", 'Apllication/Json');
-    const {method, url } = req
+const server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse<IncomingMessage>) => {
+    res.setHeader("Content-Type", "Application/Json");
+    const { method, url } = req;
     let status: number = 404;
 
-    let response : iMessage = {
-        mesage: 'failed',
-        success: false,
-        data: null
-    }
-
-    const Container: any = []
-
-    req.on ('data', (chink: any)=>{
-        Container.push(chink)
-    })
-
-    req.on('end', ()=>{
-        if(url  === '/'&& method === 'GET'){
-            status = 200
-            response.mesage = 'Success';
-            response.success = true;
-            response.data = set07;
-            res.write(JSON.stringify((response)));
-            res.end()
+    let response: iMessage = {
+      message: "failed",
+      success: false,
+      data: null,
+    };
+    const container: any = [];
+    req
+      .on("data", (chunk: any) => {
+        container.push(chunk);
+      })
+      .on("end", () => {
+  
+        if (url === "/" && method === "GET") {
+          status = 200;
+          response.message = "All set08 data gotten";
+          response.success = true;
+          response.data = set08;
+          res.write(JSON.stringify({ response, status }));
+          res.end();
         }
 
-        if (url === '/' && method === 'POST'){
-            status = 201
-            const body = JSON.parse(Container)
-            set07.push(body)
-            response.mesage = 'Success';
-            response.success = true;
-            response.data = set07;
-            res.write(JSON.stringify((response)));
-            res.end()
+        if (url === "/" && method === "POST") {
+          status = 201;
+          const body = JSON.parse(container);
+          set08.push(body);
+          response.message = "SUCCESSFULLY added";
+          response.success = true;
+          response.data = set08;
+          res.write(JSON.stringify({ response, status }));
+
+          res.end();
         }
-    })
+      });
+  }
+);
 
-})
-
-
-Server.listen (port, ()=>{
-    console.log(`Server is up and running on port: ${port}`);
-    
-})
+server.listen(port, () => {
+  console.log("Server is up and running");
+});
